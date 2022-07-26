942e8da8708ca5705fdd624c5d551e29719b1f11
voltagit <pitawat76@gmail.com>
2022-07-26 09:57:30 +0700

    Changes to be committed: 	modified:   src/App.js 	modified:   src/features/board/Board.js 	modified:   src/features/board/boardSlice.js 	modified:   src/features/board/cardRow/CardRow.js 	new file:   src/features/board/cardRow/card/Card.js 	modified:   src/features/score/Score.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import './App.css';
 import React from 'react';
 import { Score } from './features/score/Score.js';
 import { Board } from './features/board/Board.js';
-// Add import statements below
 import { useDispatch } from 'react-redux'; 
 import { setBoard, resetCards } from './features/board/boardSlice.js'; 
 
 
 const App = () => {
-  // Add dispatch variable below
   const dispatch = useDispatch();
 
   const startGameHandler = () => {
-    // Add action dispatch below
     dispatch(setBoard()); 
   };
 
   const tryAgainHandler = () => {
-    // Add action dispatch below
     dispatch(resetCards()); 
   };
 
---
+++ src/features/board/cardRow/card/Card.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useSelector } from 'react-redux'; 
+import { selectVisibleIDs, flipCard, selectMatchedIDs, resetCards } from '../../boardSlice.js';
+import { useDispatch } from 'react-redux'; 
+
+let cardLogo = "https://static-assets.codecademy.com/Courses/Learn-Redux/matching-game/codecademy_logo.png";
+
+export const Card = ({ id, contents }) => {
+  const visibleIDs = useSelector(selectVisibleIDs);
+  const dispatch = useDispatch(); 
+  const matchedIDs = useSelector(selectMatchedIDs);
+  
+  const flipHandler = (id) => {
+    dispatch(flipCard(id)); 
+  };
+
+  const tryAgainHandler = () => {
+    dispatch(resetCards()); 
+  };
+
+  let cardStyle = 'resting'
+  let click = () => flipHandler(id);
+  
+  let cardText = (
+    <img src={cardLogo} className="logo-placeholder" alt="Card option" />
+  );
+
+  if (visibleIDs.includes(id) || matchedIDs.includes(id)) {
+    cardText = contents;
+    click = () => {};
+  }
+
+  if (matchedIDs.includes(id)) {
+    cardStyle = 'matched';
+  }
+
+  if (visibleIDs.length === 2) {
+    click = tryAgainHandler;
+  }
+
+  if (visibleIDs.length >= 2 && !matchedIDs.includes(id)) {
+    cardStyle = 'no-match'; 
+  }
+
+  return (
+    <button onClick={click} className={`card ${cardStyle}`}>
+      {cardText}
+    </button>
+  );
+};
